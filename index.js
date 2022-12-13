dfdaeda18899b360b2d8f8815338a92bd1ebe8d7
guavalines <konstrictor@gmail.com>
2022-12-12 18:26:42 -0800

    added days constant

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const monthE1 = document.querySelector(".date h1");
 const fullDateE1 = document.querySelector(".date p");
+const daysE1 = document.querySelector(".days");
 
 const monthInx = new Date().getMonth();
-const lastDay = new Date(new Date().getFullYear(), monthInx, 0);
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
 console.log(lastDay);
 const months = [
   "January",
@@ -21,3 +22,11 @@ const months = [
 
 monthE1.innerText = months[monthInx];
 fullDateE1.innerText = new Date().toDateString();
+
+let days = "";
+
+for (let i = 1; i <= lastDay; i++) {
+  days += `<div>${i}</div>` ;
+}
+
+daysE1.innerHTML = days;
