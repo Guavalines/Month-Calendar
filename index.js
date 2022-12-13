26acd2ff4214507653811251a7700c106658e3c0
guavalines <konstrictor@gmail.com>
2022-12-13 00:39:49 -0800

    cleaned up js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const daysE1 = document.querySelector(".days");
 const monthInx = new Date().getMonth();
 const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
 const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
-console.log(firstDay);
+
 
 const months = [
   "January",
